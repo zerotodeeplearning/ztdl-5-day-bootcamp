88f5abbc2053d15aa369b5868311923a8ea27a8a
Francesco Mosconi <git@mosconi.me>
2018-12-14 13:45:09 -0800

    download update to zip

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install zip
 pushd ~
-find ./ztdl-5-day-bootcamp -name "*.ipynb" | tar -czvf to_download.tgz -T -
+zip -R ./ztdl-5-day-bootcamp '*.ipynb'
+# find ./ztdl-5-day-bootcamp -name "*.ipynb" | tar -czvf to_download.tgz -T -
 popd
